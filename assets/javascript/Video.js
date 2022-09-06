fbaf85b16b622c63213ea1749c9c44a5014bb5ff
Artem <105991680+m0nza@users.noreply.github.com>
2022-09-06 22:28:12 +0500

    Promo site v2

---
+++ assets/javascript/Video.js
@@ -0,0 +1,36 @@
+//
+// Скрипт видеоплеера
+//
+
+document.getElementById('main').style.display = 'none';
+let mySource = document.getElementById(`myVideo`);
+
+// Параметры плеера
+
+let videoLength = 1; // количество видео файлов
+let pathVideo = `assets/video-bg/`; // путь до видео
+let indexTag = `` // например background1.mp4, в данном случае background - tag
+let fileFormat = '.mp4' // формат видоефайлов
+mySource.volume = 0.3; // изменения громкости видео
+mySource.disablePictureInPicture = true; // отключение функции "Окно в окне"
+
+//
+//
+//
+
+let X = document.getElementById('X')
+X.addEventListener('click', videoPlay);
+function videoPlay() {
+	X.style.display = 'none';
+	document.getElementById('main').style.display = 'block';
+	let index = Math.ceil(Math.random() * videoLength);
+	mySource.setAttribute(`src`, pathVideo + indexTag + index + fileFormat);
+	document.getElementById('myVideo').play();
+	document.removeEventListener('click', videoPlay);
+}
+
+let video = document.getElementById("myVideo");
+video.onloadeddata = function () {
+	document.getElementById(`menu`).style.filter = `none`;
+	document.getElementById(`menu`).style.animation = "animate 1s linear";
+}
